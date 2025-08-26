f09b5c538b70bc791f520b8cde356213b5088de4
Parish Gawari <parishk1024@gmail.com>
2025-08-26 08:27:02 +0530

    updated the cors policy for prod.

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
