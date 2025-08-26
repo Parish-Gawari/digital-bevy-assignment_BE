dc5e0e0c740d6deaa8701f5eb086511c656d246e
Parish Gawari <parishk1024@gmail.com>
2025-08-26 09:52:44 +0530

    updated the cors option

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(cors({
   origin: process.env.CLIENT_URL,
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH', 'OPTIONS'],
   credentials: true
 }));
 app.use(express.json());
