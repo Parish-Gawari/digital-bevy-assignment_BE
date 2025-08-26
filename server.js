124913494fdc70d3ffca7a5afd3b9539a238ad5e
Parish Gawari <parishk1024@gmail.com>
2025-08-26 21:21:33 +0530

    removed logs and support for options

--- server.js
+++ server.js
@@ -16,8 +16,6 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"], 
 }));
 
-
-app.options("*", cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
@@ -26,7 +24,6 @@ app.use("/api/search", searchRoutes);
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    console.log('For Debigging Purpose Only to check whether the latest code is being deployed or not');
     app.listen(process.env.PORT || 5000, () => {
       console.log(`Server running on port ${process.env.PORT || 5000}`);
     });
