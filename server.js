ae00d335b5595f993399f95e32dc3cdca8c6b365
Parish Gawari <parishk1024@gmail.com>
2025-08-26 08:08:46 +0530

    removed comments

--- server.js
+++ server.js
@@ -10,15 +10,12 @@ dotenv.config();
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/search", searchRoutes);
 
-// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
