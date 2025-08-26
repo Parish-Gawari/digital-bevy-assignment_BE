dfdb3856bc44627da4450cb371d8acd5f1b927c9
Parish Gawari <parishk1024@gmail.com>
2025-08-26 21:53:54 +0530

    added cors origin with methods

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://digital-bevy.netlify.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  credentials: true
+}));
 
 app.use(express.json());
 
